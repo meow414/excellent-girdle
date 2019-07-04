909a48f2ab1a1748a07b6bddbf0fc1af40c8145b
Glitch (excellent-girdle) <none>
2019-07-04 11:41:22 +0000

    🌝😜 Checkpoint ./server.js:5325173/123

--- server.js
+++ server.js
@@ -20,8 +20,9 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+   var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
+  res.json({greeting: ip});
 });
 
 
